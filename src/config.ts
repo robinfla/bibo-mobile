24eb6efc65ab300e63a95425610f7c6be2636432
Robin Flamant <rob.flamant@gmail.com>
2026-02-26 19:27:17 +0000

    Temp: point API to HTTP endpoint while HTTPS routing is fixed

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 const getBaseUrl = () => {
-  return 'https://cellar.zubi.wine'
+  // TEMP: Use HTTP while HTTPS routing is being fixed
+  return 'http://46.225.12.54:3000'
+  // return 'https://cellar.zubi.wine'
 }
 
 export const API_BASE_URL = getBaseUrl()
